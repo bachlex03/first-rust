6b405e1901ef47ea362be28ce74add4f6c524e7e
bachlex1608 <lxbachit03.working@gmail.com>
2025-05-21 15:10:26 +0700

    init rust experience

---
+++ main.rs
@@ -0,0 +1,8 @@
+
+// native rust code
+// This is a simple Rust program that prints "Hello, world!" to the console.
+// run `rustc main.rs` to compile the code
+// and `./main` to run the compiled program.
+fn main() {
+    println!("Hello, world!");
+}
